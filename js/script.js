6421603ad9013fa90a9711196d9e147d6344b31d
Maxim <maximguwop@icloud.com>
2023-01-22 19:44:22 +0300

    add-dark-mode

--- js/script.js
+++ js/script.js
@@ -3,4 +3,19 @@ const miuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
-const toggleEl = document.querySelector('.toggle');
+const toggle = document.querySelector('.toggle');
+
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+"Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        e.target.innerHTML ='Dark mode';
+    } else {
+        html.classList.add('dark');
+        e.target.innerHTML ='Light mode';
+    }
+});
